02a35cab9e39452ac14485df49054ed12d98d58b
Colin David Scott <colin@abstractcode.com>
2020-08-25 23:09:14 +1000

    More backoff testing

--- runner/src/test/scala/com/abstractcode/eventrunner/BackoffSpec.scala
+++ runner/src/test/scala/com/abstractcode/eventrunner/BackoffSpec.scala
@@ -2,16 +2,23 @@ package com.abstractcode.eventrunner
 
 import cats.effect.concurrent.Ref
 import cats.effect.{Clock, IO, Sync, Timer}
-import org.specs2.Specification
+import org.scalacheck.Prop
 import org.specs2.matcher.MatchResult
+import org.specs2.scalacheck.{ScalaCheckFunction1, ScalaCheckFunction2}
 import org.specs2.specification.Tables
 import org.specs2.specification.core.SpecStructure
+import org.specs2.{ScalaCheck, Specification}
 
 import scala.concurrent.duration.{DAYS, Duration, FiniteDuration, SECONDS}
 
-class BackoffSpec extends Specification with Tables {
+class BackoffSpec extends Specification with ScalaCheck with Tables {
   def is: SpecStructure =
     s2"""
+         backoff should
+            return action result $returnActionResult
+            sleep for backoff function response $sleepForBackoffFunctionResponse
+            store new backoff value $storeNewBackoffValue
+
          linearBackoff should
             store updated backoff ${
       "initial" | "increment" | "maximum" | "expected" |>
@@ -30,15 +37,68 @@ class BackoffSpec extends Specification with Tables {
         (initial, expected) => shouldSleepForInitialBackoff(initial, expected)
       }
     }
+
           Can reset $shouldReset
       """
 
-  def linearBackoffStoreUpdatedBackoff(initial: FiniteDuration, increment: FiniteDuration, maximum: FiniteDuration, expected: FiniteDuration): MatchResult[Any] = {
-    implicit val timer: Timer[IO] = new Timer[IO] {
-      def clock: Clock[IO] = throw new NotImplementedError()
+  val doNothingTimer: Timer[IO] = new Timer[IO] {
+    def clock: Clock[IO] = throw new NotImplementedError()
 
-      def sleep(duration: FiniteDuration): IO[Unit] = IO.unit
+    def sleep(duration: FiniteDuration): IO[Unit] = IO.unit
+  }
+
+  val buildTimerRef: Ref[IO, FiniteDuration] => Timer[IO] = s => new Timer[IO] {
+    def clock: Clock[IO] = throw new NotImplementedError()
+    def sleep(duration: FiniteDuration): IO[Unit] =  s.set(duration)
+  }
+
+  def returnActionResult: ScalaCheckFunction1[Int, MatchResult[Any]] =
+    prop {
+      (expected: Int) =>
+        implicit val timer: Timer[IO] = doNothingTimer
+
+        val result = for {
+          ref <- Ref.of[IO, Unit](())
+          r <- Backoff.backoff[IO, Int, Unit](_ => ((), Duration.Zero))(ref)(IO.pure(expected))
+        } yield r
+
+        result.unsafeRunSync() shouldEqual expected
     }
+
+  def sleepForBackoffFunctionResponse: ScalaCheckFunction1[FiniteDuration, MatchResult[Any]] =
+    prop {
+      (sleepDuration: FiniteDuration) =>
+        val result = for {
+          ref <- Ref.of[IO, Unit](())
+          sleep <- Ref.of[IO, FiniteDuration](FiniteDuration(-10, DAYS))
+          _ <- Backoff.backoff((_: Unit) => ((), sleepDuration))(ref)(IO.unit)(buildTimerRef(sleep), Sync[IO])
+          value <- sleep.get
+        } yield value
+
+        result.unsafeRunSync() shouldEqual sleepDuration
+    }
+
+  def storeNewBackoffValue: ScalaCheckFunction2[Int, Int, Prop] =
+    prop {
+      (initial: Int, expected: Int) =>
+        (initial != expected) ==> {
+          implicit val timer: Timer[IO] = doNothingTimer
+
+          val result = for {
+            ref <- Ref.of[IO, Int](initial)
+            _ <- Backoff.backoff[IO, Unit, Int] {
+              case `initial` => (expected, Duration.Zero)
+              case _ => (initial, Duration.Zero)
+            }(ref)(IO.pure(()))
+            value <- ref.get
+          } yield value
+
+          result.unsafeRunSync() shouldEqual expected
+        }
+    }
+
+  def linearBackoffStoreUpdatedBackoff(initial: FiniteDuration, increment: FiniteDuration, maximum: FiniteDuration, expected: FiniteDuration): MatchResult[Any] = {
+    implicit val timer: Timer[IO] = doNothingTimer
 
     val result = for {
       ref <- Ref.of[IO, FiniteDuration](initial)
@@ -50,15 +110,10 @@ class BackoffSpec extends Specification with Tables {
   }
 
   def shouldSleepForInitialBackoff(initial: FiniteDuration, expected: FiniteDuration): MatchResult[Any] = {
-    val timer: Ref[IO, FiniteDuration] => Timer[IO] = s => new Timer[IO] {
-      def clock: Clock[IO] = throw new NotImplementedError()
-      def sleep(duration: FiniteDuration): IO[Unit] =  s.set(duration)
-    }
-
     val result = for {
       ref <- Ref.of[IO, FiniteDuration](initial)
       sleep <- Ref.of[IO, FiniteDuration](FiniteDuration(-10, DAYS))
-      _ <- Backoff.linearBackoff(FiniteDuration(60, SECONDS))(FiniteDuration(5, SECONDS))(ref)(IO.unit)(timer(sleep), Sync[IO])
+      _ <- Backoff.linearBackoff(FiniteDuration(60, SECONDS))(FiniteDuration(5, SECONDS))(ref)(IO.unit)(buildTimerRef(sleep), Sync[IO])
       value <- sleep.get
     } yield value
 
